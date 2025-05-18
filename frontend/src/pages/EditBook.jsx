8178c1b5e7ff6845aef88d22d67ff4ce3892272e
HafsahTariq-03 <175113561+HafsahTariq-03@users.noreply.github.com>
2025-05-19 02:09:04 +0500

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -16,7 +16,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`http://localhost:5555/books/${id}`)
+    axios.get(`http://54.252.182.58:5555/books/${id}`)
     .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear)
